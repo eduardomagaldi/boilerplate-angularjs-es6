dcd42238470ea464161ca9686c16e3e4e16064d4
Eduardo Magaldi <magaldi1989@gmail.com>
2018-01-01 01:25:10 -0200

    cleaning code

--- components/main/js/tests/tests.js
+++ components/main/js/tests/tests.js
@@ -7,12 +7,8 @@ import 'mocha';
 import chai from 'chai';
 import sinon from 'sinon';
 
-console.log('mocha', mocha);
-
 window.sinon = sinon;
 window.chai = chai;
-console.log('chai', chai);
-console.log('window', window);
 
 mocha.setup({
 	'ui': 'bdd',
@@ -21,38 +17,9 @@ mocha.setup({
 
 require('angular-mocks');
 
-// import * as angular from 'angular';
-// import 'angular-mocks';
-
-// describe('some component', () => {
-
-//     beforeEach(() => {
-//         angular.mock.module('someModule');
-//     });
-
-// });
-
-
-
 require('./main.test');
 
 require('./states.helper.test');
 require('./components.helper.test');
 
 mocha.run();
-
-// import 'bootstrap/js/dropdown';
-// import 'bootstrap/js/collapse';
-
-// import './js/app';
-// import './js/config';
-// import './js/run';
-
-// function loadScripts() {
-// 	var script = document.createElement('script');
-// 	script.onload = function() {
-// 	  alert("Script loaded and ready");
-// 	};
-// 	script.src = "http://whatever.com/the/script.js";
-// 	document.getElementsByTagName('head')[0].appendChild(script);
-// }
